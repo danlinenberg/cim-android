f0d9fa5bf9a6c105c1448b8a1a8446de2ae0f96a
Dan Linenberg <dan.linenberg@appsflyer.com>
2017-06-10 00:29:01 +0300

    added rooms

--- app/src/main/java/com/tau/dtr/cim_application/MultiplayerManager.java
+++ app/src/main/java/com/tau/dtr/cim_application/MultiplayerManager.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -13,7 +15,15 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.Room;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
 import com.google.example.games.basegameutils.BaseGameUtils;
+
+import java.util.List;
 
 import static com.tau.dtr.cim_application.Utils.Utils.log;
 
@@ -21,11 +31,10 @@ import static com.tau.dtr.cim_application.Utils.Utils.log;
  * Created by dan on 08/06/2017.
  */
 
-public class MultiplayerManager extends FragmentActivity implements ResultCallback, GoogleApiClient.OnConnectionFailedListener {
+public class MultiplayerManager extends FragmentActivity implements ResultCallback, GoogleApiClient.OnConnectionFailedListener,RealTimeMessageReceivedListener, RoomUpdateListener {
 
     public static MultiplayerManager mContext = new MultiplayerManager();
-    int RC_SELECT_PLAYERS = 2;
-    private GoogleApiClient mGoogleApiClient;
+    public static GoogleApiClient mGoogleApiClient;
     private static int RC_SIGN_IN = 9001;
 
     private boolean mResolvingConnectionFailure = false;
@@ -41,7 +50,7 @@ public class MultiplayerManager extends FragmentActivity implements ResultCallba
                     .addConnectionCallbacks( new GoogleApiClient.ConnectionCallbacks() {
                         @Override
                         public void onConnected(Bundle connectionHint) {
-                            log("Connected");
+                            log("Connected to gamecenter");
                             StartGame();
                         }
                         @Override
@@ -69,10 +78,6 @@ public class MultiplayerManager extends FragmentActivity implements ResultCallba
             mSignInClicked = false;
             mResolvingConnectionFailure = true;
 
-            // Attempt to resolve the connection failure using BaseGameUtils.
-            // The R.string.signin_other_error value should reference a generic
-            // error string in your strings.xml file, such as "There was
-            // an issue with sign-in, please try again later."
             if (!BaseGameUtils.resolveConnectionFailure(mContext,
                     mGoogleApiClient, connectionResult,
                     RC_SIGN_IN, R.string.sign_in_other_error)) {
@@ -81,6 +86,21 @@ public class MultiplayerManager extends FragmentActivity implements ResultCallba
         }
 
         log("Need to login");
+    }
+
+    public void StartGame(){
+        Bundle am = RoomConfig.createAutoMatchCriteria(1, 1, 0);
+
+        // build the room config:
+        RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+        roomConfigBuilder.setAutoMatchCriteria(am);
+        RoomConfig roomConfig = roomConfigBuilder.build();
+
+        // create room:
+        Games.RealTimeMultiplayer.create(mGoogleApiClient, roomConfig);
+
+        // prevent screen from sleeping during handshake
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     public void OnButtonLogin(View v){
@@ -96,8 +116,10 @@ public class MultiplayerManager extends FragmentActivity implements ResultCallba
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent intent) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        /**
+         * Sign in
+         */
         if (requestCode == RC_SIGN_IN) {
             mSignInClicked = false;
             mResolvingConnectionFailure = false;
@@ -129,8 +151,110 @@ public class MultiplayerManager extends FragmentActivity implements ResultCallba
         log("Res: " + result);
     }
 
-    public void StartGame(){
+    @Override
+    public void onRealTimeMessageReceived(RealTimeMessage realTimeMessage) {
 
+    }
+
+    private RoomConfig.Builder makeBasicRoomConfigBuilder() {
+        return RoomConfig.builder(this)
+                .setMessageReceivedListener(this)
+                .setRoomStatusUpdateListener(new RoomStatusUpdateListener() {
+                    @Override
+                    public void onRoomConnecting(Room room) {
+                        log("Connecting to room");
+                        showToast("Connecting to room");
+                    }
+
+                    @Override
+                    public void onRoomAutoMatching(Room room) {
+                        log("Connecting to room");
+                    }
+
+                    @Override
+                    public void onPeerInvitedToRoom(Room room, List<String> list) {
+
+                    }
+
+                    @Override
+                    public void onPeerDeclined(Room room, List<String> list) {
+
+                    }
+
+                    @Override
+                    public void onPeerJoined(Room room, List<String> list) {
+                        log("Peer joined");
+                    }
+
+                    @Override
+                    public void onPeerLeft(Room room, List<String> list) {
+
+                    }
+
+                    @Override
+                    public void onConnectedToRoom(Room room) {
+                        showToast("Connected to room");
+                        log("Connected to room");
+                    }
+
+                    @Override
+                    public void onDisconnectedFromRoom(Room room) {
+                        showToast("Disconnected");
+                        log("Disconnected from room");
+                    }
+
+                    @Override
+                    public void onPeersConnected(Room room, List<String> list) {
+
+                    }
+
+                    @Override
+                    public void onPeersDisconnected(Room room, List<String> list) {
+
+                    }
+
+                    @Override
+                    public void onP2PConnected(String s) {
+                        showToast("P2P connected");
+                        log("P2P Connected");
+                    }
+
+                    @Override
+                    public void onP2PDisconnected(String s) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void onRoomCreated(int i, Room room) {
+        log("Created room #"+i);
+    }
+
+    @Override
+    public void onLeftRoom(int i, String s) {
+        log("Left room #"+i);
+    }
+
+    @Override
+    public void onJoinedRoom(int i, Room room) {
+        log("Joined room  #" +i);
+    }
+
+    @Override
+    public void onRoomConnected(int i, Room room) {
+        log("Connected room #" + i);
+    }
+
+
+    public void showToast(final String txt)
+    {
+        runOnUiThread(new Runnable() {
+            public void run()
+            {
+                Toast.makeText(getApplicationContext(),txt, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
